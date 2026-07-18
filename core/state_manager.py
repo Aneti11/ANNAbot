ab88fe918cf1280ac3e111a29d771646d5101478
Пользователь Intel <kuchuguraanna@gmail.com>
2026-07-18 12:41:41 +0200

    Extend StateManager data storage

--- core/state_manager.py
+++ core/state_manager.py
@@ -1,10 +1,22 @@
 class StateManager:
     def __init__(self):
-        self.state = "STARTING"
+        self.system_state = "STARTING"
+
+        self.data = {
+            "account": None,
+            "current_module": None,
+            "window": None
+        }
 
     def set_state(self, new_state):
-        self.state = new_state
-        print(f"[STATE] {self.state}")
+        self.system_state = new_state
+        print(f"[STATE] {self.system_state}")
 
     def get_state(self):
-        return self.state
+        return self.system_state
+
+    def set_data(self, key, value):
+        self.data[key] = value
+
+    def get_data(self, key):
+        return self.data.get(key)
